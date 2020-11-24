0c7d8a2c339b23b5c9bb39e3f09f0a0dda0e17c6
Alexey <alexx10@mail.ru>
2020-11-24 07:21:25 +0300

    finish lesson 49, added jquery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: "https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/280x178_2"
+      ),
+
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'misha'
+      )
+    ]
   end
 
   def new
@@ -14,5 +28,12 @@ class UsersController < ApplicationController
       username: 'installero',
       avatar_url: "https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/280x178_2"
     )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('23.11.2020')),
+      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('23.11.2020'))
+    ]
+
+    @new_question = Question.new
   end
 end
