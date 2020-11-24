ae8b934f90cf54a06133df518ad71161ad11ec82
Alexey <alexx10@mail.ru>
2020-11-24 19:58:58 +0300

    add questions count field

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def questions_form_count(q_count)
+    q_count %= 100 if q_count > 100
+    q_count %= 10 if q_count > 20
+
+    return "#{q_count} вопрос" if q_count == 1
+    return "#{q_count} вопроса" if q_count.between?(2, 4)
+    "#{q_count} вопросов"
+  end
 end
