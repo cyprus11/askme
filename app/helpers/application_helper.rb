071bb7e36ea331edea8afc077988afb3505298c2
Alexey <alexx10@mail.ru>
2020-11-26 23:58:58 +0300

    fixed sklonyator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,14 +7,14 @@ module ApplicationHelper
     end
   end
 
-  def questions_form_count(q_count)
+  def sklonyator(q_count, word_forms)
     q_count %= 100 if q_count > 100
     q_count %= 10 if q_count > 20
 
-    return "#{q_count} вопрос" if q_count == 1
-    return "#{q_count} вопроса" if q_count.between?(2, 4)
+    return "#{q_count} #{word_forms[0]}" if q_count == 1
+    return "#{q_count} #{word_forms[1]}" if q_count.between?(2, 4)
 
-    "#{q_count} вопросов"
+    "#{q_count} #{word_forms[2]}"
   end
 
   def fa_icon(icon_class)
