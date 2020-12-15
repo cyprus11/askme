67e165e389addb70ff98a9783ee0c12ad6715fdf
Alexey <alexx10@mail.ru>
2020-12-15 21:07:57 +0300

    security fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :questions, except: [:new, :index, :show]
-  resources :hashtags, only: [:index, :show]
-
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
+  resource :session, only: %i[new create destroy]
+  resources :questions, except: %i[new index show]
+  resources :hashtags, only: %i[index show]
 end
